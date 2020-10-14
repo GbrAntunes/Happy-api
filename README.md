bc6c6788acc5d16dd76b306c99c6d02c9705addf
Gabriel Antunes <gbr.antunes@gmail.com>
2020-10-14 11:33:32 -0400

    API finalizada no segundo ep da NLW #03

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Happy API
+#### Scripts
+- `dev`
+- `typeorm`
+
+#### Rotas disponíveis
+- Criar orfanatos
+  `[POST] /orphanages`
+  - name
+  - latitude
+  - longitude
+  - about
+  - instructions
+  - opening_hours
+  - open_on_weekends
+  - images
+
+- Listar orfanatos
+  `[GET] /orphanages`
+
+- Buscar orfanato por id
+  `[GET] /orphanages/:id`
--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
 import Orphanage from '../models/Ophanage'
 import orphanageView from '../views/orphanages_view'
+import * as Yup from 'yup'
 
 export default {
   async create(req: Request, resp: Response) {
@@ -20,11 +21,12 @@ export default {
     // O Multer tem um problema de tipagem e o req.files não pode ser trabalhado como array
     // sendo assim, precisamos forçar a tipagem de um array de File
     const requestImages = req.files as Express.Multer.File[]
+
     const images = requestImages.map(image => {
       return { path: image.filename }
     })
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -33,7 +35,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images,
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      ),
     })
+
+    await schema.validate(data, {
+      abortEarly: false,
+    })
+
+    const orphanage = orphanagesRepository.create()
   
     await orphanagesRepository.save(orphanage)
   
@@ -47,7 +70,7 @@ export default {
       relations: ['images']
     })
 
-    return resp.json(orphanages)
+    return resp.json(orphanageView.renderMany(orphanages))
   },
   
   async show(req: Request, resp: Response) {
